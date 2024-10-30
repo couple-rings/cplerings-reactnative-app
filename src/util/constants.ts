949f9f8937a297f303dadea4fb866f0752cea971
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2024-10-30 21:56:54 +0700

    MOBILE-2.1: Build login screen: Completed

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,2 +1,8 @@
 export const primaryColor = "#080E29";
+
 export const secondaryColor = "#888";
+
+export const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export const passwordPattern =
+  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
